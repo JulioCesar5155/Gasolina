ff03b9ff094f2401a9ec728d714c8e4d255b8b76
JulioCesar5155 <125457479+JulioCesar5155@users.noreply.github.com>
2023-09-05 18:41:43 +0000

    Gasolina

--- App.js
+++ App.js
@@ -3,23 +3,33 @@ import { useState } from 'react';
 import { StyleSheet, Text, View, TextInput } from 'react-native';
 
 export default function App() {
-  const [valorLitro, setValorLitro] = useState ('')
-  const [qtdAbastecimento, setValorAbastecimento] = useState ('')
-  const [kmRodados, setValorKm ] = useState ('')
-  const [preço, setPreço ] = useState ('')
+  const [valorLitro, setValorLitro] = useState ('6.50')
+  const [qtdAbastecimento, setValorAbastecimento] = useState ('10')
+  const [kmRodados, setValorKm ] = useState ('100')
+  const [preco, setPreco ] = useState ('')
+  const [resultado, setResultado ] = useState ('')
 
 
   const calcularLitro = (litro) => {
     let intLitro = parseInt(valorLitro, 10);
     let intAbastecimento = parseInt(qtdAbastecimento, 10);
-    setPreço(intAbastecimento * intLitro);
+    let intKms = parseInt(kmRodados, 10);
+    let intPreco = parseInt(preco, 10)
+    setPreco(intAbastecimento * intLitro);
+    setResultado(intPreco / intKms);
     Keyboard.dismiss()
   };
+
+
 
   return (
     <View style={styles.container}>
       <Text>Valor do litro</Text>
-      <TextInput></TextInput>
+      <TextInput value={valorLitro} onChangeText={setValorLitro}></TextInput>
+      <Text>Quantidade abastecida</Text>
+      <TextInput value={qtdAbastecimento} onChangeText={setValorAbastecimento}></TextInput>
+      <Text>Km rodados</Text>
+      <TextInput value={kmRodados} onChangeText={setValorKm}></TextInput>
     </View>
   );
 }
