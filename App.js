74bc719c00cc216bdfed437204706a5ecb3ce127
JulioCesar5155 <125457479+JulioCesar5155@users.noreply.github.com>
2023-09-05 18:41:53 +0000

    Gasolina

--- App.js
+++ App.js
@@ -1,35 +1,70 @@
 import { StatusBar } from 'expo-status-bar';
-import { useState } from 'react';
-import { StyleSheet, Text, View, TextInput } from 'react-native';
+import React, { useState } from 'react';
+import { StyleSheet, Text, View, TextInput, Pressable, Keyboard } from 'react-native';
 
 export default function App() {
-  const [valorLitro, setValorLitro] = useState ('6.50')
-  const [qtdAbastecimento, setValorAbastecimento] = useState ('10')
-  const [kmRodados, setValorKm ] = useState ('100')
-  const [preco, setPreco ] = useState ('')
-  const [resultado, setResultado ] = useState ('')
+  const [valorLitro, setValorLitro] = useState('6.50');
+  const [qtdAbastecimento, setValorAbastecimento] = useState('10');
+  const [kmRodados, setValorKm] = useState('100');
+  const [resultado, setResultado] = useState('');
 
+  const calcularLitro = () => {
+    const intLitro = parseFloat(valorLitro);
+    const intAbastecimento = parseFloat(qtdAbastecimento);
+    const intKms = parseFloat(kmRodados);
 
-  const calcularLitro = (litro) => {
-    let intLitro = parseInt(valorLitro, 10);
-    let intAbastecimento = parseInt(qtdAbastecimento, 10);
-    let intKms = parseInt(kmRodados, 10);
-    let intPreco = parseInt(preco, 10)
-    setPreco(intAbastecimento * intLitro);
-    setResultado(intPreco / intKms);
-    Keyboard.dismiss()
+    const intPreco = intAbastecimento * intLitro;
+    const consumo = intPreco / intKms;
+    setResultado(consumo.toFixed(2));
+    Keyboard.dismiss();
   };
-
-
 
   return (
     <View style={styles.container}>
-      <Text>Valor do litro</Text>
-      <TextInput value={valorLitro} onChangeText={setValorLitro}></TextInput>
-      <Text>Quantidade abastecida</Text>
-      <TextInput value={qtdAbastecimento} onChangeText={setValorAbastecimento}></TextInput>
-      <Text>Km rodados</Text>
-      <TextInput value={kmRodados} onChangeText={setValorKm}></TextInput>
+      <Text style={styles.title}>Consumo de Combustível</Text>
+
+      <View style={styles.inputContainer}>
+        <Text style={styles.label}>Valor do litro</Text>
+        <TextInput
+          style={styles.input}
+          value={valorLitro}
+          onChangeText={setValorLitro}
+          keyboardType='numeric'
+        />
+      </View>
+
+      <View style={styles.inputContainer}>
+        <Text style={styles.label}>Quantidade abastecida</Text>
+        <TextInput
+          style={styles.input}
+          value={qtdAbastecimento}
+          onChangeText={setValorAbastecimento}
+          keyboardType='numeric'
+        />
+      </View>
+
+      <View style={styles.inputContainer}>
+        <Text style={styles.label}>Km rodados</Text>
+        <TextInput
+          style={styles.input}
+          value={kmRodados}
+          onChangeText={setValorKm}
+          keyboardType='numeric'
+        />
+      </View>
+
+      <Pressable style={styles.button} onPress={calcularLitro}>
+        <Text style={styles.buttonText}>Calcular</Text>
+      </Pressable>
+
+      {resultado !== '' && (
+        <View style={styles.resultContainer}>
+          <Text style={styles.resultLabel}>Consumo Médio:</Text>
+          <Text style={styles.resultText}>{resultado} R$/KM</Text>
+        </View>
+      )}
+
+      <StatusBar style="auto" />
     </View>
   );
 }
@@ -37,8 +72,58 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#F5F5F5',
     alignItems: 'center',
     justifyContent: 'center',
+    paddingHorizontal: 20,
+  },
+  title: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    marginBottom: 20,
+  },
+  inputContainer: {
+    marginBottom: 10,
+  },
+  label: {
+    fontSize: 18,
+    marginBottom: 5,
+    textAlign: 'center'
+  },
+  input: {
+    backgroundColor: '#FFFFFF',
+    borderRadius: 5,
+    padding: 10,
+    fontSize: 16,
+    textAlign: 'center',
+    width: 200,
+    height: 50,
+  },
+  button: {
+    backgroundColor: '#007AFF',
+    padding: 15,
+    borderRadius: 5,
+    marginTop: 20,
+    alignItems: 'center',
+  },
+  buttonText: {
+    color: '#FFFFFF',
+    fontSize: 18,
+    fontWeight: 'bold',
+  },
+  resultContainer: {
+    backgroundColor: '#FFFFFF',
+    borderRadius: 5,
+    padding: 15,
+    marginTop: 20,
+    alignItems: 'center',
+  },
+  resultLabel: {
+    fontSize: 16,
+  },
+  resultText: {
+    fontSize: 18,
+    fontWeight: 'bold',
+    marginTop: 5,
   },
 });
